8d83d4173be680ee2021e2e24909ab9d9209e016
hisacro <hisacro@tuta.io>
2020-03-12 22:53:00 +0530

    end case when i/p isn't num

--- guessinggame.sh
+++ guessinggame.sh
@@ -7,14 +7,14 @@ echo "guess no of files"
 function guessing {
 while [[ $guess -ne $no ]]; do
 	read guess 
-	if [[ $guess -eq $no ]]
-	then
+	if [[ -n ${guess//[0-9]/} ]]; then
+		echo "input a number"
+		guess=-1
+	elif [[ $guess -eq $no ]]; then
 		echo "correct guess"
-	elif [[ $((guess-no)) -gt 5 ]]
-	then
+	elif [[ $((guess-no)) -gt 5 ]]; then
 		echo "very high"
-	elif [[ $((no-guess)) -gt 5 ]]
-	then
+	elif [[ $((no-guess)) -gt 5 ]]; then
 		echo "very low"
 	else
 		echo "almost there"		
